7f9e0e7973accae4f34077f0bfdc44a60184eef9
erikasolli8405 <arboldorado@gmail.com>
2025-03-31 14:30:55 +0200

    Update prosjekt_oppgave.py

--- prosjekt_oppgave.py
+++ prosjekt_oppgave.py
@@ -12,25 +12,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file = "support_uke_24.xlsx"  #navn til filen
+file = "support_uke_24.xlsx"  
 data = pd.read_excel(file) # lese filen
 #print(data)
 
 # Hente ut data frå spesifikke kolonner
 
-u_dag = data["Ukedag"].values              #lagrer data fra kolonne 1 (ukedag) en array
-kl_slett = data["Klokkeslett"].values      #lagrer data fra kolonne 2 (Klokkeslett) en array
-varighet = data["Varighet"].values         #lagrer data fra kolonne 3 (Samtales varighet) i en array
-score = data["Tilfredshet"].values         #lagrer data fra kolonne 4 (Kundens tilfredshet) i en array      
+u_dag = data["Ukedag"].values             
+kl_slett = data["Klokkeslett"].values     
+varighet = data["Varighet"].values         
+score = data["Tilfredshet"].values             
 
 
 #Del A. Skrive et program som finner antall henvendelser for hver de 5 ukedagene. Resultatet viser i et søylediagram.
 #Antall henvendelser per ukedag
 
-antall_henvendelser_per_dag = data["Ukedag"].value_counts()                    # Antall henvendelser for hver ukedag
-ukedager = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag"]                # Sørger for at ukedagene vises i en spesifikk rekkefølge
-antall_henvendelser_per_dag = antall_henvendelser_per_dag[ukedager].fillna(0)  # Hvis noen ukedager mangler, kan vi sikre at alle er med, 
-                                                                               # selv om det ikke er noen henvendelser
+antall_henvendelser_per_dag = data["Ukedag"].value_counts()                    
+ukedager = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag"]                
+antall_henvendelser_per_dag = antall_henvendelser_per_dag[ukedager].fillna(0)  
+                                                                               
 
 plt.bar(antall_henvendelser_per_dag.index, antall_henvendelser_per_dag.values)
 plt.xlabel("Ukedag")
@@ -41,8 +41,8 @@ plt.show()
 
 #Del C. Skriv et program som finner minste og lengste samtaletid som er loggført for uke 24.
 
-min_tid = np.min(varighet)                                                      # Minste samtaletid
-max_tid = np.max(varighet)                                                      # Lengste samtaletid
+min_tid = np.min(varighet)                                                      
+max_tid = np.max(varighet)                                                      
 
 
 #print(f"Den korteste samtale var {min_tid} minutter.")
@@ -76,7 +76,7 @@ def kategoriser_tid(tid):
         return "Utenfor tidsintervall"                                          # Hvis det er utenfor de ønskede tidsintervallene
     
     
-data["Tidsintervall"] = data["Klokkeslett"].apply(kategoriser_tid)            # Bruk funksjonen på hver rad for å kategorisere klokkeslettene
+data["Tidsintervall"] = data["Klokkeslett"].apply(kategoriser_tid)              # Bruk funksjonen på hver rad for å kategorisere klokkeslettene
 
 total_antall_henvendelser_per_bolk = data["Tidsintervall"].value_counts()       # Finn antall henvendelser for hvert tidsintervall
 
@@ -91,11 +91,11 @@ plt.show()
 data = data.dropna(subset=["Tilfredshet"])                                     #Fjerne NaN-verdier fra tilfredshetsscoren
 data["Tilfredshet"] = data["Tilfredshet"].astype(int)                          #Konvertere data til tilfredshet til heltall
 
-antall_positive = sum((data["Tilfredshet"] >= 9) & (data["Tilfredshet"] <= 10))# Beregne antall positive kunder
-antall_noytrale = sum((data["Tilfredshet"] >= 7) & (data["Tilfredshet"] <= 8)) # Beregne antall nøytrale kunder
-antall_negative = sum((data["Tilfredshet"] >= 1) & (data["Tilfredshet"] <= 6)) # Beregne antall negative kunder
+antall_positive = sum((data["Tilfredshet"] >= 9) & (data["Tilfredshet"] <= 10))
+antall_noytrale = sum((data["Tilfredshet"] >= 7) & (data["Tilfredshet"] <= 8)) 
+antall_negative = sum((data["Tilfredshet"] >= 1) & (data["Tilfredshet"] <= 6)) 
 
-totale_tilbakemeldinger = antall_positive + antall_noytrale + antall_negative   # Totalt antall tilbakemeldinger
+totale_tilbakemeldinger = antall_positive + antall_noytrale + antall_negative   
 
 prosent_positive = (antall_positive / totale_tilbakemeldinger)* 100             # Beregne prosentandel 
 prosent_negative =(antall_negative / totale_tilbakemeldinger) * 100  
